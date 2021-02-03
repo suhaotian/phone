c659df18c261e4862c9e5d03b837c49bdd2dcfde
Your Name <you@example.com>
2021-02-03 22:39:07 +0800

    prettier files

--- scripts/mv.js
+++ scripts/mv.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const dir = path.join(__dirname, '../dist/iso3166Data');
+const dir = path.join(__dirname, "../dist/iso3166Data");
 if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir, 0744);
+  fs.mkdirSync(dir, 0744);
 }
 
-fs.readdir(path.join(__dirname, '../iso3166Data'), (err, files) => {
-	files.forEach((file) => {
-		fs.copyFileSync(path.join(__dirname, '../iso3166Data', file), dir + '/' + file);
-	});
+fs.readdir(path.join(__dirname, "../iso3166Data"), (err, files) => {
+  files.forEach((file) => {
+    fs.copyFileSync(
+      path.join(__dirname, "../iso3166Data", file),
+      dir + "/" + file,
+    );
+  });
 });
